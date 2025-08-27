96481f04cf731e75c7af1590987b7a1c778253a4
Leon Windhorst <leon@windhorst.dev>
2025-08-27 12:42:28 +0200

    Add runtime dependencies to LD_LIBRARY_PATH

--- flake.nix
+++ flake.nix
@@ -13,17 +13,30 @@
     in {
       devShell.${system} = pkgs.mkShell {
         packages = with pkgs; [
+          cmake
+          ninja
           pkg-config
 
-          cmake
-          llvmPackages.clang-unwrapped
-          ninja
+          clang-tools
 
           vulkan-headers
           vulkan-loader
           vulkan-memory-allocator
           vulkan-utility-libraries
+
+          # wayland dependencies for SDL
+          libffi
+          libGL
+          libxkbcommon
+          wayland
+          wayland-protocols
+          wayland-scanner
         ];
+
+        shellHook = ''
+          export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.libxkbcommon}/lib
+          export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.wayland}/lib
+        '';
       };
     };
 }
